af0c2718bf17e4c541166be2eb729ad245a18e20
Patricia <patriciamvbog@gmail.com>
2023-09-27 16:02:17 +0100

    add template react and icon window

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+/* IMPORT SECTION */
+
+// - From React
+// - Our
+// - Sass
+import '../styles/App.scss';
+// - Images
+
+/* COMPONENT SECTION */
+function App() {
+  /* STATE VARIABLES (DATA) */
+
+  /* EFFECTS (code when the page loads) */
+
+  /* HANDLER FUNCTIONS */
+
+  /* FUNCTIONS AND AUXILIARY VARIABLES TO PAINT THE HTML */
+
+  /* HTML */
+  return <div className="App">
+      <header>
+        <h1>Blodd Match</h1>
+      </header>
+  </div>;
+}
+
+/* PROP-TYPES */
+
+/* EXPORT */
+export default App;
